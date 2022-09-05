c9f3e301dd41f42b6931289d792f6dd75b2f8faf
michellelestuny <112867186+michellelestuny@users.noreply.github.com>
2022-09-05 19:37:26 +0700

    UG2_71190415

---
+++ michelle ug 2.py
@@ -0,0 +1,37 @@
+import random
+import timeit
+
+#rekrusif
+    
+def fibonacci (n):
+  if n < 1:
+    return [n]
+
+  return fibonacci(n - 1) + [n]
+
+for i in range(1,101):
+    start = timeit.default_timer()
+    hasil = fibonacci(i)
+    end = timeit.default_timer()
+    print('Ukuran ', hasil, ': ', end-start, ' second.')
+
+#iteratif
+    
+FibArray = [0, 1]
+ 
+def fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+    elif n < len(FibArray):
+        return FibArray[n]
+    else:       
+        FibArray.append(fibonacci(n - 1) + fibonacci(n - 2))
+        return FibArray[n]
+
+for i in range(1,101):
+    start = timeit.default_timer()
+    hasil = fibonacci(i)
+    end = timeit.default_timer()
+    print('Ukuran ', hasil, ': ', end-start, ' second.')
+
+              
